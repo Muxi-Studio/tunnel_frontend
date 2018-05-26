d1f3f225cb5f0f019f5e22fd40dccefb874193d6
darkglimmer <2392897209@qq.com>
2018-05-26 10:35:10 +0800

    time

---
+++ Time/src/components/control.vue
@@ -0,0 +1,106 @@
+<template>
+<div>
+    <el-date-picker
+      v-model="value1"
+      type="date"
+      placeholder="选择日期">
+    </el-date-picker>
+    </div>
+ <el-table :data="tableData" style="width: 100%">
+   <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column
+        prop="date"
+        label="日期"
+        sortable
+        width="180"
+        :filters="[{text: '2016-05-01', value: '2016-05-01'}, {text: '2016-05-02', value: '2016-05-02'}, {text: '2016-05-03', value: '2016-05-03'}, {text: '2016-05-04', value: '2016-05-04'}]"
+      :filter-method="filterHandler"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="id"
+        label="ID"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="time"
+        label="发送时间"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="e-mail"
+        label="发送地址"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="words"
+        label="想说的话"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        label="状态">
+      </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">发送</el-button>
+        <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table> 
+  <div class="block" style="width: 100%;">
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      layout="total, prev, pager, next, jumper"
+      :page-size="25"
+      :total="100">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+   methods: {
+      handleEdit(index, row){
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      },
+       filterHandler(value, row, column) {
+        const property = column['property'];
+        return row[property] === value;
+      },
+       handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+      
+    },
+    data() {
+      return {
+         value1: '',
+         tableData: [{
+          date: '2016-05-02',
+          id: '王小虎',
+          status:'已发送'
+        }, {
+          date: '2016-05-04',
+          id: '王小虎',
+          status:'已发送'
+        }, {
+          date: '2016-05-01',
+          id: '王小虎',
+          status:'未发送'
+        }, {
+          date: '2016-05-03',
+          id: '王小虎',
+          status:'已发送'
+        }]
+      }
+    }
+  };
+</script>
---
+++ Time/src/components/time.vue
@@ -0,0 +1,106 @@
+<template>
+<div id = "title">时空隧道</div>
+<div id = "introduce">
+    <p>一段心声+目标邮箱+未来某个日期+随意昵称=时空之匙</p>
+    <p>给自己一个期冀，一次机会，一份勇气</p>
+    <p>你想对未来的我或TA说什么？</p>
+</div>
+<div id = "send">
+    <div>发送时间</div>
+    <div id = "choicetime">
+    <el-date-picker
+      v-model="value1"
+      type="date"
+      placeholder="选择日期">
+    </el-date-picker>
+    </div>
+</div>
+<div id = "send">
+    <div>我要发给</div>
+    <input type="text" id = "email"/>
+</div>
+<div id = "send">
+    <div>想说的话</div>
+    <div id = "content">内容要求：
+        <p>(1)对侮辱性、诽谤性、违反国家规定等言论零容忍</p>
+        <p>(2)温馨提示：真的会定时发送，望三思后码字</p>
+    </div>
+    <input type="text" id = "words" />
+</div>
+<div id = "send">
+    <div>昵称
+        <p>可以在这儿备注上自己的名字或者昵称</p>
+    </div>
+    <input type="text" id = "ID" />
+</div>
+<button  type="button" id = "decide">提交</button>
+<div id = "muxi">Powered_by_木犀团队</div>
+
+
+</template>
+<script>
+export default {
+    data() {
+        return {
+            value1: '',
+        };
+    }
+};
+</script>
+<style scoped>
+#title{
+    margin: 0 auto;
+    width: 42%;
+    height: 65px;
+    margin-top: 120px;
+    font-size: 40px;
+    color:#90dbdc;
+}
+#introduce{
+    margin: 0 auto;
+    color: white;
+    font-size: 26px;
+    margin-top: 190px;
+}
+#send{
+    margin: 0 auto;
+    font-size: 32px;
+    color: white;
+    margin: 0 auto;
+    margin-top: px;
+}
+#choicetime{
+    width: 78%;
+    height: 88px;
+}
+#content{
+
+}
+#email{
+    width: 78%;
+    height: 88px;
+}
+#words{
+    width: 78%;
+    height: 322px;
+}
+#ID{
+    width: 78%;
+    height: 88px;
+}
+#button{
+    border-radius: 12px;
+    background-color: rgba(0, 0, 0, 0);
+    color: rgb(255, 255, 255);    
+    width: 260px;
+    height: 88px;
+    line-height: 1.2;
+    text-align: center;
+}
+#muxi{
+    font-size: 32px;
+    font-family: "SimHei";
+    color: rgb(255, 255, 255);
+    line-height: 1.2;
+}
+</style>
