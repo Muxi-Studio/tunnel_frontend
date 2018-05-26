e3e8f0ca57f14dfcf4883a287f2dc72f4acaa15f
yanyuw <547274419@qq.com>
2018-05-26 23:22:34 +0800

    feat: fetch

--- test/src/components/time.vue
+++ test/src/components/time.vue
@@ -10,7 +10,7 @@
     <div id = "send">发送时间</div>
     <div id = "choicetime">
     <el-date-picker
-      v-model="value1"
+      v-model="sent_time"
       type="date"
       placeholder="选择日期">
     </el-date-picker>
@@ -18,7 +18,7 @@
 </div>
 <div>
     <div id = "send">我要发给</div>
-    <input type="text" id = "email"/>
+    <input type="text" id = "email" v-model="sent_address"/>
 </div>
 <div>
     <div id = "send">想说的话</div>
@@ -27,16 +27,16 @@
         <p>(1)对侮辱性、诽谤性、违反国家规定等言论零容忍</p>
         <p>(2)温馨提示：真的会定时发送，望三思后码字</p>
     </div>
-    <input type="text" id = "words" />
+    <input type="text" id = "words" v-model="sent_content"/>
 </div>
 <div>
     <div id = "send">昵称</div>
     <div id = "content">
         <p>可以在这儿备注上自己的名字或者昵称</p>
     </div>
-    <input type="text" id = "ID" />
+    <input type="text" id = "ID" v-model="sent_name"/>
 </div>
-<button  type="button" id = "decide">提交</button>
+<button  type="button" id = "decide" v-on:click="sent">提交</button>
 <div id = "muxi">Powered_by_木犀团队</div>
   </div>
 </template>
@@ -45,8 +45,56 @@ export default {
     name: 'time',
     data() {
         return {
-            value1: '',
+            sent_content:"",
+            sent_time: "",
+            sent_name: "",
+            sent_address: "",
+            token:""
         };
+    },
+    mounted(){
+        fetch("http://120.79.254.77:5000/api/signin/",{
+            method: 'POST',
+            body: JSON.stringify({"username": "TStunnel","password": "Ilovemuxi"}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+            
+        }).then(res => {
+            if(res.ok){
+                return res.json();
+            }
+        }).then(res =>{
+            this.token = res.token;
+        })
+
+    },
+    methods: {
+        sent(){
+            var message = {
+                "sent_content": this.sent_content,
+                "sent_time": "123",
+                "sent_name": this.sent_name,
+                "sent_address": this.sent_name
+            };
+            fetch("/api/message/",{
+                method: 'POST',
+                body: JSON.stringify(message),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            }).then(res => {
+                if(res.ok){
+                    return res.json();
+                } else {
+                    // console.log("x");
+                    this.failed = true;
+                }
+            }).then(value =>{
+                //
+            })
+        }
     }
 };
 </script>
